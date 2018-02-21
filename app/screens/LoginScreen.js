8dad4712a631a5b023d22657a64aac085958fc60
motifej <motifej@users.noreply.github.com>
2018-02-21 18:20:59 +0200

    before react-navigator (#1)

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { StackNavigator } from 'react-navigation';
-import LinearGradient from 'react-native-linear-gradient';
 import {
   Picker,
   StyleSheet,
   TextInput,
   Text,
   View,
+  Alert,
 } from 'react-native';
+import Header from '../components/Header';
 import { login } from '../actions/user';
 
 class LoginScreen extends React.Component {
 
   state = {
     phone: '+7',
-    count: 0,
-    colorTop: '#000000',
-    colorBottom: '#cccccc',
-  }
+  };
 
   componentDidMount() {
     this.props.login(100);
@@ -27,23 +24,21 @@ class LoginScreen extends React.Component {
   render() {
     return (
       <View>
-      <LinearGradient
-      colors={['#E70F62', '#FC5A14']}
-      start={{x: 1, y: 0}} end={{x: 0, y: 0}}
-      style={styles.gradient} >
+        <Header
+          title="Sign in"
+          goBack={() => Alert('goBack')}
+        />
         <TextInput
           style={styles.input}
           value={this.state.phone}
           onChangeText={this._onChangeText}
           placeholder="Enter your phone"
-          autoFocus={true}
           autoCorrect={true}
           keyboardType="numeric"
           returnKeyType="next"
           onSubmitEditing={this._submit}
           blurOnSubmit={false}
         />
-        </LinearGradient>
       </View>
     );
   }
@@ -54,10 +49,6 @@ class LoginScreen extends React.Component {
     alert(`Phone ${this.state.phone}`);
   };
 }
-
-export default connect(null, {
-  login: login.post.request,
-})(LoginScreen);
 
 const styles = StyleSheet.create({
   input: {
@@ -70,30 +61,8 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     fontSize: 16,
   },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  gradient: {
-    width: 200,
-    height: 200,
-  },
-  linearGradient: {
-    flex: 1,
-    paddingLeft: 15,
-    paddingRight: 15,
-    borderRadius: 5
-  },
-  buttonText: {
-    fontSize: 18,
-    fontFamily: 'Gill Sans',
-    textAlign: 'center',
-    margin: 10,
-    color: '#6b52ae',
-    backgroundColor: 'transparent',
-  },
 });
 
-
+export default connect(null, {
+  login: login.post.request,
+})(LoginScreen);
